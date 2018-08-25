3048fe39f11272b984879efab3b78679ef5a5d3c
Ben Mulvey <ben@mulveyfamily.com>
2018-08-24 23:36:58 -0400

    random messages

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import conf
+import random
 
 
 client = discord.Client()
@@ -15,6 +16,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    number = random.randint(0,999)
     if message.content.find('F') != -1:
 
         if str(message.author) == "DJ Mitch#4397" or str(message.author) == "PressFBot#3434":
@@ -22,6 +24,8 @@ async def on_message(message):
         else:
 
             await client.send_message(message.channel, 'Press F to pay respects')
+    elif number == 0:
+        await client.send_message(message.channel, 'Press F to pay respects')
 
 
 client.run(conf.key)
